e005e79f77d2fc430dd223ac9f44c5136d960597
zater ahmed <124626957+ahmed123456787@users.noreply.github.com>
2026-02-18 23:02:50 +0100

    add admin user

--- iftar_backend/wsgi.py
+++ iftar_backend/wsgi.py
@@ -8,9 +8,23 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+from django.contrib.auth import get_user_model
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iftar_backend.settings')
+
+# This will run ONCE when the app starts
+django.setup()
+
+# Create superuser if it doesn't exist
+User = get_user_model()
+if not User.objects.filter(is_superuser=True).exists():
+    User.objects.create_superuser(
+        username='admin',           # Change this
+        email='admin@example.com',  # Change this
+        password='admin123'         # Change this
+    )
+    print("✅ Superuser created successfully!")
 
 application = get_wsgi_application()
